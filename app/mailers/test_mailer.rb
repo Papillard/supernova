a66a2f9420990c505df2d877ed8eef41b12eec35
Boris Paillard <papillard@MacBook-Pro-de-Boris.local>
2025-12-22 14:04:10 +0100

    Add TestMailer for SMTP test

--- app/mailers/test_mailer.rb
+++ app/mailers/test_mailer.rb
@@ -1,12 +1,9 @@
 class TestMailer < ApplicationMailer
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.test_mailer.ping.subject
-  #
-  def ping
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def ping(to)
+    mail(
+      to: to,
+      subject: "ProfConnect SMTP OK",
+      body: "ping"
+    )
   end
 end
